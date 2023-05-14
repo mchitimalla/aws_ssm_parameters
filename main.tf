da92522755d5b9371a5bf8e7691fdb1e07dd76ad
Monika <monika.chitimalla@gmail.com>
2023-05-14 12:14:39 +0100

    Learning

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_ssm_parameter" "parameters" {
 }
 resource "aws_ssm_parameter" "passwords" {
   count = length(var.passwords)
-  name = var.passwords[count.index].value
+  name = var.passwords[count.index].name
   type = "SecureString"
   value = var.passwords[count.index].value
   key_id = "bab54d3f-2462-4a3f-b32c-59f05ca38d9d"
